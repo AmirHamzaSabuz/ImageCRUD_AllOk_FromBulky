b8a874750a3ee8004029987ed0a1c2e7d4c11013
Amir Hamza <hamza_amir@yahoo.com>
2023-01-10 15:10:58 +0600

    comment

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace Experiment_Image_Bulky.Controllers
         }
         public IActionResult Index()
         {
-                var objProductList = _context.Products.ToList();
+            var objProductList = _context.Products.ToList();
             return View(objProductList);
         }
 
@@ -32,7 +32,7 @@ namespace Experiment_Image_Bulky.Controllers
             ProductVM productVM = new()
             {
                 Product = new(),
-                
+
             };
             if (id == null || id == 0)
             {
@@ -50,7 +50,7 @@ namespace Experiment_Image_Bulky.Controllers
 
         }
 
-        
+
 
         //POST
         [HttpPost]
@@ -64,31 +64,31 @@ namespace Experiment_Image_Bulky.Controllers
                     string wwwRootPath = _hostEnvironment.WebRootPath;
                     //if (file != null)
                     //{
-                        string fileName = Guid.NewGuid().ToString();
-                        var uploads = Path.Combine(wwwRootPath, @"images\products");
-                        var extension = Path.GetExtension(file.FileName);
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
 
 
-                        //if (obj.Product.ImageUrl != null) // For Create ImageURL always Null
-                        //{
-                        //    var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
-                        //    if (System.IO.File.Exists(oldImagePath))
-                        //    {
-                        //        System.IO.File.Delete(oldImagePath);
-                        //    }
-                        //}
+                    //if (obj.Product.ImageUrl != null) // For Create ImageURL always Null
+                    //{
+                    //    var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                    //    if (System.IO.File.Exists(oldImagePath))
+                    //    {
+                    //        System.IO.File.Delete(oldImagePath);
+                    //    }
+                    //}
 
-                        using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                        {
-                            file.CopyTo(fileStreams);
-                        }
-                        obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
 
                     //}
 
                     //if (obj.Product.Id == 0)  // For Create Id always 0
                     //{
-                        _context.Products.Add(obj.Product);
+                    _context.Products.Add(obj.Product);
                     //}
                     //else
                     //{
@@ -114,12 +114,12 @@ namespace Experiment_Image_Bulky.Controllers
 
                         //if (obj.Product.ImageUrl != null) // for update always imageUrl Exist or Not null
                         //{
-                            var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
-                            if (System.IO.File.Exists(oldImagePath))
-                            {
-                                System.IO.File.Delete(oldImagePath);
-                            }
-                       // }
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                        // }
 
                         using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                         {
@@ -135,7 +135,7 @@ namespace Experiment_Image_Bulky.Controllers
                     //}
                     //else // For Update Id always Not 0.
                     //{
-                        _context.Products.Update(obj.Product);
+                    _context.Products.Update(obj.Product);
                     //}
 
                     _context.SaveChanges();
@@ -145,40 +145,41 @@ namespace Experiment_Image_Bulky.Controllers
             }
 
             return View(obj);
-        
+
         }
 
         /*
-         * For Create If File Exist, No Url Exist
-         * For Create If File Not Exist. No URL Exist
-         * For Create URL Always Null. So No URL Delete Code.
-         * 
-         * For Create If File Exist, No URL Delete Code, Because There is No Previous URL. Write New URL.
-         * For Create If File Not Exist, Not Valid.
-         * 
-         * For Edit/Update If File Exist, Url Exist
-         * For Edit/Update If File Not exist, Url Exist.
-         * For Edit/Update URL Always Exist. 
-         * 
-         * All Url Code Works When File Exist or Not Null
-         * All Url Code Will Not Work When File Not Exist Or Null. 
-         * 
-         * For Edit/Update If File Exist or Not Null, All URL Code will work. Delete Previous URL. Make New URL.
-         * For Edit/Update If File Not Exist or Null, All URL Code will not work. Update/Save with previous URL. No Delete No Write
-         * 
-         * */
+        * For Create If File Exist, No Url Exist
+        * For Create If File Not Exist. No URL Exist
+        * For Create URL Always Null. So No URL "Delete" Code.
+        * 
+        * For Create If File Exist, No URL Delete Code, Because There is No Previous URL. Write New URL.
+        * For Create If File Not Exist, Not Valid.
+        * 
+        * For Edit/Update If File Exist, Url Exist
+        * For Edit/Update If File Not exist, Url Exist.
+        * For Edit/Update URL Always Exist. 
+        * 
+        * For Edit/Update All Url Code Works When File Exist. Or Not Null
+        * For Edit/Update All Url Code Will Not Work When File Not Exist. Or Null. 
+        * 
+        * For Edit/Update If File Exist or Not Null, All URL Code will work. Delete Previous URL. Make New URL.
+        * For Edit/Update If File Not Exist or Null, All URL Code will not work. Update/Save with previous URL. No Delete No Write.
+        * 
+        * */
+
 
         /*
-         * For Create, If File Not Exist, Invalid
-         * For Create, If File Exist, Don't Delete URL, Because there is No Previus URL. Write New URL.
-         * 
-         * 
-         * For Edit, If New File Not Exist, Previous Url Exist. Don't Delete Previous Url. Don't Write New Url. Save/Update with Previous Url.
-         * For Edit, If New File Exist, Previous Url Exist. Delete Previous Url.  Write New Url. Save/Update with New Url.
-         * */
+        * For Create, If File Not Exist, Invalid
+        * For Create, If File Exist, Don't Delete URL, Because there is No Previus URL. Write New URL.
+        * 
+        * 
+        * For Edit, If New File Not Exist, Previous Url Exist. Don't Delete Previous Url. Don't Write New Url. Save/Update with Previous Url.
+        * For Edit, If New File Exist, Previous Url Exist. Delete Previous Url.  Write New Url. Save/Update with New Url.
+        * */
 
 
-        
+
 
         [HttpGet]
         public IActionResult Delete(int id)
